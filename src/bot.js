6091712d7db9549cc9593da9310fd711f261bc84
Jody LeCompte <jody@jodylecompte.com>
2023-06-27 13:23:29 -0500

    More bugs to squash -- but iterates and blocks users automatically now

--- src/bot.js
+++ src/bot.js
@@ -1,11 +1,25 @@
 require('dotenv').config();
 
 const puppeteer = require('puppeteer');
-const fs = require('fs');
 
-async function loginTwitter() {
+const execute = async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
+
+  await twitterLogin(page);
+
+  while (true) {
+    const advertisers = new Set();
+    await findAdvertisers(page, advertisers);
+    await blockAdvertisers(page, advertisers);
+
+    await page.waitForTimeout(3000);
+    await page.goto('https://twitter.com/home');
+    // await page.waitForNavigation();
+  }
+}
+
+const twitterLogin = async (page) => {
   await page.goto('https://twitter.com/login');
 
   await page.waitForSelector('input[autocomplete="username"]');
@@ -31,15 +45,18 @@ async function loginTwitter() {
       loginButton.click();
     }
   });
+}
 
-  await page.waitForNavigation();
-  await page.waitForTimeout(5000);
-  const advertisers = new Set();
+const findAdvertisers = async (page, advertisers) => {
+  // await page.goto('https://twitter.com/home');
+  // await page.waitForNavigation();
 
-  while (advertisers.size < 10) {
+  while (advertisers.size < 1) {
     await page.evaluate(() => {
       window.scrollBy(0, window.innerHeight);
     });
+
+    await page.waitForTimeout(5000);
 
     const tweetUrls = await page.evaluate(() => {
       const tweetNodes = Array.from(document.querySelectorAll('article'));
@@ -51,7 +68,10 @@ async function loginTwitter() {
           anchorTags.forEach(anchor => {
             const url = anchor.getAttribute('href');
             if (url && url.match(/^\/[a-zA-Z0-9_]+$/)) {
-              urls.push(url.replace('/', ''));
+              const handle = url.replace('/', '');
+              if (!urls.includes(handle)) {
+                urls.push(handle);
+              }
             }
           });
         }
@@ -61,11 +81,43 @@ async function loginTwitter() {
     });
 
     tweetUrls.forEach(url => {
-      advertisers.add(url);
+      if (!advertisers.has(url)) {
+        advertisers.add(url);
+        console.log('Advertiser Identified: ', `@${url}`)
+      }
     });
   }
-
-  console.log(advertisers);
 }
 
-loginTwitter();
+const blockAdvertiser = async (page, advertiser) => {
+  const menuSelector = 'div[data-testid="userActions"]';
+
+  try {
+    page.goto(`https://twitter.com/${advertiser}`);
+  } catch (e) {
+    console.log(e)
+  }
+  await page.waitForNavigation();
+  await page.waitForSelector(menuSelector);
+
+  await page.evaluate((advertiser) => {
+    // Main user actions menu
+    document.querySelector('div[data-testid="userActions"]').click();
+    // Block Button
+    Array.from(document.querySelectorAll('span')).find(element => element.textContent === `Block @${advertiser}`).click();
+    // Block confirmation button
+    Array.from(document.querySelectorAll('span')).find(element => element.textContent === 'Block').click();
+  }, advertiser);
+
+  console.log(`Blocked: @${advertiser}`)
+
+  await page.waitForTimeout(3000);
+}
+
+const blockAdvertisers = async (page, advertisers) => {
+  for (const advertiser of advertisers) {
+    await blockAdvertiser(page, advertiser);
+  }
+};
+
+execute();
